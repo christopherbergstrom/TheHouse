a0e57392d204c95c1d29ce949a15e20f9ac831c4
Christopher Bergstrom <christopherrbergstrom@gmail.com>
2016-07-10 16:52:25 -0600

    back to where I was before

--- theHouseNew.js
+++ theHouseNew.js
@@ -8,8 +8,8 @@ var body;
 // var messageBtn;
 
 var loc="Downstairs Center Room";
-var inventory;
-var message;
+var inventory="";
+var message="";
 
 $(document).ready(function()
 {
@@ -31,14 +31,103 @@ $(document).ready(function()
       });
     });
   });
+  $("#inventoryBtn").click(function()
+  {
+    $("body").append("<div id='readme'></div>").hide().fadeIn("slow");
+    $("#readme").append("<div id='readmeExit' class='fa fa-times'></div>");
+    $("#readme").append("<div id='readmeTitle'>Inventory</div>");
+    $("#readme").append("<div id='readmeText'>"+inventory+"</div>");
+    $("#readmeExit").click(function()
+    {
+      $("#readme").fadeOut(300, function()
+      {
+        $(this).remove();
+      });
+    });
+  });
+  $("#messageBtn").click(function()
+  {
+    $("body").append("<div id='readme'></div>").hide().fadeIn("slow");
+    $("#readme").append("<div id='readmeExit' class='fa fa-times'></div>");
+    $("#readme").append("<div id='readmeTitle'>Message</div>");
+    $("#readme").append("<div id='readmeText'>"+message+"</div>");
+    $("#readmeExit").click(function()
+    {
+      $("#readme").fadeOut(300, function()
+      {
+        $(this).remove();
+      });
+    });
+  });
 });
 function createDownCenter()
 {
-  container.append("<button class='up buttons'>Down North</button>");
-  container.append("<button class='right buttons'>Down East</button>");
-  container.append("<button class='down buttons'>Down South</button>");
-  container.append("<button class='left buttons'>Down West</button>");
-  container.append("<button class='center buttons'>Upstairs</button>");
+  loc="Downstairs Center Room";
+  container.append("<div id='current'></div>")
+  $("#current").append("<button class='up buttons'>Down North</button>");
+  $("#current").append("<button class='right buttons'>Down East</button>");
+  $("#current").append("<button class='down buttons'>Down South</button>");
+  $("#current").append("<button class='left buttons'>Down West</button>");
+  $("#current").append("<button class='center buttons'>Upstairs</button>");
+  $(".up").click(function()
+  {
+    messageCheck();
+    $("#current").remove();
+    createDownNorth();
+  });
+  $(".right").click(function()
+  {
+    messageCheck();
+    if (inventory.indexOf("door key") != -1)
+    {
+      $("#current").remove();
+      createDownEast();
+    }
+    else
+    {
+      message="This door is locked, a door key is needed.";
+    }
+  });
+  $(".down").click(function()
+  {
+    messageCheck();
+    $("#current").remove();
+    createDownSouth();
+  });
+  $(".left").click(function()
+  {
+    messageCheck();
+    $("#current").remove();
+    createDownWest();
+  });
+  $(".center").click(function()
+  {
+    messageCheck();
+    $("#current").remove();
+    createUpCenter();
+  });
+}
+function createDownNorth()
+{
+  loc="Downstairs North Room";
+  container.append("<div id='current'></div>")
+  $("#current").append("<button class='down buttons'>Down Center</button>");
+  if (inventory.indexOf("saw") === -1)
+  {
+    $("#current").append("<button class='obj'>saw</button")
+    $(".obj").click(function()
+    {
+      messageCheck();
+      inventory+="saw, ";
+      $(".obj").remove();
+    });
+  }
+  $(".down").click(function()
+  {
+    messageCheck();
+    $("#current").remove();
+    createDownCenter();
+  });
 }
 function locationColor()
 {
